c754c31c8a71d8002ddac1882357de75d4da3fa8
Serhii Posternak <serpos95@gmail.com>
2019-08-22 00:42:38 +0300

    task solved

--- problem-solving/PlusMinus.php
+++ problem-solving/PlusMinus.php
@@ -1,1 +1,29 @@
 <?php
+
+// Complete the plusMinus function below.
+function plusMinus($arr) {
+    $count = count($arr);
+    $plus = 0;
+    $minus = 0;
+    array_walk($arr, function($item) use (&$plus, &$minus) {
+        if ($item > 0) {
+            ++$plus;
+        } elseif ($item < 0) {
+            ++$minus;
+        }
+    });
+
+    print number_format($plus/$count, 6)."\n".number_format($minus/$count, 6)."\n".number_format(($count-$plus-$minus)/$count, 6);
+}
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%d\n", $n);
+
+fscanf($stdin, "%[^\n]", $arr_temp);
+
+$arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+plusMinus($arr);
+
+fclose($stdin);
